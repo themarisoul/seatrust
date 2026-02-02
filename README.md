e7c1087b6a2b2059f532acab8eb9061893673d57
tv-23 <tvelqjiri@gmail.com>
2026-02-02 17:08:08 +0100

    gfgf

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-# seatrust
-=======
 # Astro Starter Kit: Basics
 
 ```sh
@@ -47,4 +44,3 @@ All commands are run from the root of the project, from a terminal:
 ## 👀 Want to learn more?
 
 Feel free to check [our documentation](https://docs.astro.build) or jump into our [Discord server](https://astro.build/chat).
->>>>>>> e25e0c3 (kjkj)
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import keystatic from '@keystatic/astro';
-import react from '@astrojs/react';
-import node from '@astrojs/node';
 
-import markdoc from '@astrojs/markdoc';
-
-export default defineConfig({
-  output: 'static', // Keystatic gérera les routes serveur automatiquement
-  integrations: [react(), keystatic(), markdoc()],
-  adapter: node({
-    mode: "standalone"
-  }),
-  vite: {
-    ssr: {
-      // Pour corriger l'erreur de "lodash" vue plus tôt
-      noExternal: ['@keystatic/core', '@keystatic/astro', 'lodash']
-    }
-  }
-});
+// https://astro.build/config
+export default defineConfig({});
